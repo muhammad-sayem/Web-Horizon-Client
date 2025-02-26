0ba8a3d9aaf0fbd340b4c92776f7cb8ee724b2e9
muhammad-sayem <shahrulislamsayem@gmail.com>
2025-02-26 22:52:38 +0600

    server link added

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://server-three-indol-46.vercel.app',
 })
 
 const useAxiosSecure = () => {
